f9dc0d9ec106f3fd422d7eec340ac15a6ee69ebe
Mr-Zer00 <49174975+Mr-Zer00@users.noreply.github.com>
2021-07-29 10:47:04 -0300

    Update Pass-Gen.py

--- Pass-Gen.py
+++ Pass-Gen.py
@@ -6,7 +6,7 @@ numbers = "0123456789"
 symbols = "[]{}()!@#$%&*-+=:;.,"
 
 all = lower + upper + numbers + symbols
-length = 16 # Pode ser alterado para quantos caracters quiser
+length = 16 # O valor pode ser alterado para quantos caracters quiser
 password = "".join(random.sample(all, length))
 
 print(password)
