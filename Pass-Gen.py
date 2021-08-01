dae935febdf321e724b265f2d8561ebeb14ca98d
Mr-Zer00 <49174975+Mr-Zer00@users.noreply.github.com>
2021-08-01 19:57:43 -0300

    Update Pass-Gen.py

--- Pass-Gen.py
+++ Pass-Gen.py
@@ -1,9 +1,10 @@
 import random
 
-lower = "abcdefghijklmnopqrstuvwxyzç"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower = "abcdefghijklmnopqrstuvwxyzç" # Em algumas aplicaçãoes, não é permitido o "ç".
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ"  # Em algumas aplicaçãoes, não é permitido o "Ç".
 numbers = "0123456789"
-symbols = "[]{}()!@#$%&*-+=:;.,"
+symbols = "[]{}()!@#$%&*-+=:;.,"    # Em algumas aplicaçãoes, não é permitido alguns caracters.
+numbers = "0123456789"
 
 all = lower + upper + numbers + symbols
 length = 16 # O valor pode ser alterado para quantos caracters quiser
